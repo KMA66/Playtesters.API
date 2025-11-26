a0fcd93ef20930962f62f026b74e0febbdd06d9d
MrDave1999 <daveseva2010@hotmail.es>
2025-11-25 19:41:49 -0500

    test: add unit tests for GetTestersValidator

---
+++ tests/Validators/GetTestersValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation.TestHelper;
+using Playtesters.API.UseCases.Testers;
+
+namespace Playtesters.API.Tests.Validators;
+
+public class GetTestersValidatorTests
+{
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("   ")]
+    [TestCase("CreatedAtDesc")]
+    [TestCase("createdatdesc")]
+    [TestCase("TotalHoursPlayedAsc")]
+    [TestCase("totalhoursplayedasc")]
+    public void OrderBy_WhenValidValue_ShouldNotHaveValidationErrors(string orderBy)
+    {
+        // Arrange
+        var validator = new GetTestersValidator();
+        var request = new GetTestersRequest(OrderBy: orderBy);
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(r => r.OrderBy);
+    }
+
+    [TestCase("InvalidValue")]
+    [TestCase("1234")]
+    [TestCase("///")]
+    [TestCase("CreatedAtDescending")]
+    public void OrderBy_WhenInvalidValue_ShouldHaveValidationError(string orderBy)
+    {
+        // Arrange
+        var validator = new GetTestersValidator();
+        var request = new GetTestersRequest(OrderBy: orderBy);
+        var allowed = string.Join(", ", Enum.GetNames<GetTestersOrderBy>());
+        var expectedMessage = $"Invalid orderBy '{orderBy}'. Allowed values: {allowed}";
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(r => r.OrderBy)
+              .WithErrorMessage(expectedMessage);
+    }
+}
